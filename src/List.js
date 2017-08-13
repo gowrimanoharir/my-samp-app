453670c729ac2f10a08c7565da6187f487b4bac2
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-13 18:52:16 -0500

    added prettier

--- src/List.js
+++ src/List.js
@@ -1,37 +1,36 @@
-import React from "react";
-import {ListDiv, ListItem, ListHeader, Ulist, DelButton} from './style';
+import React from 'react';
+import { ListDiv, ListItem, ListHeader, Ulist, DelButton } from './style';
 
+class List extends React.Component {
+  constructor(props) {
+    super(props);
 
-class List extends React.Component{
-    constructor(props){
-        super(props);
-        //this.state = {myArrList: this.props.arrList};
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-        this.handleDelete = this.handleDelete.bind(this);    
-    }
+  handleDelete(e) {
+    this.props.onDelete(e.target.parentElement.id);
+    console.log('clicked delete' + e.target.parentElement.id);
+  }
 
-    handleDelete(e){
-        this.props.onDelete(e.target.parentElement.id);
-        console.log("elicked delete"+e.target.parentElement.id);
-    }
-
-
-    render(){
-        const myArrList = this.props.arrList;
-        return (
-            <ListDiv>
-                <ListHeader>My List ({myArrList.length})</ListHeader>
-                <Ulist>    
-                    { myArrList.map((item, index) =>
-                        <ListItem id={index} key={index}>
-                            {item}
-                            <DelButton onClick = {this.handleDelete}>X</DelButton>
-                        </ListItem>
-                    )}
-                </Ulist>
-
-            </ListDiv>
-        )};
+  render() {
+    const myArrList = this.props.arrList;
+    return (
+      <ListDiv>
+        <ListHeader>
+          My List ({myArrList.length})
+        </ListHeader>
+        <Ulist>
+          {myArrList.map((item, index) =>
+            <ListItem id={index} key={index}>
+              {item}
+              <DelButton onClick={this.handleDelete}>X</DelButton>
+            </ListItem>
+          )}
+        </Ulist>
+      </ListDiv>
+    );
+  }
 }
 
 export default List;
--- src/style.js
+++ src/style.js
@@ -1,70 +1,87 @@
-import styled from '../node_modules/styled-components'
+import styled, { injectGlobal } from '../node_modules/styled-components';
+
+injectGlobal`
+    html, body {
+        font-family: sans-serif;
+        margin: 0;   
+    }
+`;
 
 const MainDiv = styled.div`
-        background: #C6E7EA;
-        padding-bottom: 5px;       
+  background: #c6e7ea;
+  padding-bottom: 5px;
+  min-height: 100%;
 `;
 
 const PageHeader = styled.h1`
-        text-align: center;
-        padding-top: 10px;
+  margin-top: 0;
+  text-align: center;
+  padding-top: 10px;
 `;
 
 const AddDiv = styled.div`
-        background: #D4D6D6;
-        display: flex; 
-        justify-content: center;
-        padding: 10px;
-        margin: 10px;
+  background: #d4d6d6;
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  margin: 10px;
 `;
 
 const InputBox = styled.input`
-        border-radius: 2px;
-        border: none;
-        margin-right: 5px;
-        width: 20%;
+  border-radius: 2px;
+  border: none;
+  margin-right: 5px;
+  width: 20%;
+  line-height: 1.5;
 `;
 
 const AddButton = styled.button`
-        border-radius: 2px;
-        background-color: #FFF;
-        border: none;
+  border-radius: 2px;
+  background-color: #fff;
+  border: none;
+  line-height: 1.5;
 `;
 
 const ListDiv = styled.div`
-        background: #ddc9a2;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 10px;
-        margin: 10px;
+  background: #ddc9a2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  margin: 10px;
 `;
 
 const ListHeader = styled.h3`
-        padding-top: 5px;
-        margin: 0px;
+  padding-top: 5px;
+  margin: 0px;
 `;
 
-const Ulist = styled.ul`
-        list-style-type: none;
-`;
+const Ulist = styled.ul`list-style-type: none;`;
 
 const ListItem = styled.li`
-        display: flex;
-        justify-content: space-between;
-        min-width: 250px;
-        background: palevioletred;
-        padding: 5px;
-        margin: 5px;
+  display: flex;
+  justify-content: space-between;
+  min-width: 250px;
+  background: palevioletred;
+  padding: 5px;
+  margin: 5px;
 `;
-
 
 const DelButton = styled.button`
-        border: none;
-        background: none;
-        font-weight: bold;
+  border: none;
+  background: none;
+  font-weight: bold;
 `;
 
-
-export {InputBox, AddDiv, AddButton, MainDiv, PageHeader, ListHeader, ListDiv, ListItem, Ulist, DelButton};
-
+export {
+  InputBox,
+  AddDiv,
+  AddButton,
+  MainDiv,
+  PageHeader,
+  ListHeader,
+  ListDiv,
+  ListItem,
+  Ulist,
+  DelButton
+};
