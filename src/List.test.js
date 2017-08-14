e9eaa63a56af5f70fcd12e7befb0862d8db4c6f6
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-13 19:18:26 -0500

    added test files

---
+++ src/List.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<List arrList={['item 1', 'item 2', 'item 3']} />, div);
+});
