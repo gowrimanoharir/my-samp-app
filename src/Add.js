64995aae9ab7da1a5a33d1fb9c53caf4c4e7111f
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-21 11:15:01 -0500

    test

--- src/Add.js
+++ src/Add.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react"
 
 //import style components necessary for this component
-import { InputBox, AddDiv, AddButton } from './style';
+import { InputBox, AddDiv, AddButton } from "./style"
 
 //import List component that will be rendered based on the added / deleted item
-import List from './List';
+import List from "./List"
 
 //create a class component to get input from user and be able to add and delete items in the list
 class Add extends React.Component {
   state = {
-    myListArr: []
-  };
+    myListArr: [],
+  }
 
   //anytime user enters data in input box the value property is set
   handleInputChange = e => {
-    this.setState({ value: e.target.value });
-  };
+    this.setState({ value: e.target.value })
+  }
 
   //when add button is clicked then the input box value is added to the array that holds the list items and then empties the input box which also updates dom with new items
   handleAdd = e => {
-    this.state.myListArr.push(this.state.value);
-    this.setState({ value: '' });
-  };
+    this.state.myListArr.push(this.state.value)
+    this.setState({ value: "" })
+  }
 
   //when delete button is clicked the index is passed from the List component which is then used to delete the item in array with help of splice and also dom is updated
   itemDelete = id => {
-    this.setState(this.state.myListArr.splice(id, 1));
-  };
+    this.setState(this.state.myListArr.splice(id, 1))
+  }
 
   //renders the dom with current state of array
   render() {
-    const arrList = this.state.myListArr;
+    const arrList = this.state.myListArr
     return (
       <div>
         <AddDiv>
@@ -44,8 +44,8 @@ class Add extends React.Component {
         </AddDiv>
         <List arrList={arrList} onDelete={this.itemDelete} />
       </div>
-    );
+    )
   }
 }
 
-export default Add;
+export default Add
