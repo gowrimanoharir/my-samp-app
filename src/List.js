085d3794f6d88c27e13dc5e31907a84878a5f7d0
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-21 11:03:44 -0500

    test

--- src/List.js
+++ src/List.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react"
 
 //import style components necessary for this component
-import { ListDiv, ListItem, ListHeader, Ulist, DelButton } from './style';
+import { ListDiv, ListItem, ListHeader, Ulist, DelButton } from "./style"
 
 //create a class component to get input from user and be able to add and delete items in the list
 class List extends React.Component {
   //this is invoked on click of delete button and in turn calls the onDelete method of Add component passing the index of the item that is stores as id in li element
   handleDelete = e => {
-    this.props.onDelete(e.target.id);
-  };
+    this.props.onDelete(e.target.id)
+  }
 
   //renders the dom with current state of array that is passed from Add component
   render() {
-    const myArrList = this.props.arrList;
+    const myArrList = this.props.arrList
     return (
       <ListDiv>
         <ListHeader>
@@ -25,12 +25,12 @@ class List extends React.Component {
               <DelButton id={index} onClick={this.handleDelete}>
                 X
               </DelButton>
-            </ListItem>
+            </ListItem>,
           )}
         </Ulist>
       </ListDiv>
-    );
+    )
   }
 }
 
-export default List;
+export default List
